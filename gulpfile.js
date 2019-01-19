75c12d9c5565d11d0ff6bec5bce83c468e8ee4a5
Steven Blom <atomicblom@binaryvibrance.net>
2019-01-20 01:01:31 +1030

    Update to new toolchain

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,9 @@
-const ModBuilder = require("minecraft-scripting-toolchain");
-const ts = require("gulp-typescript");
+const MinecraftAddonBuilder = require("minecraft-addon-toolchain/v2");
+const TypeScriptSupport = require("minecraft-addon-toolchain-typescript");
+const BrowserifySupport = require("minecraft-addon-toolchain-browserify");
 
-const builder = new ModBuilder("Chess");
-builder.scriptTasks = [
-    () => ts({
-        module: "ES6",
-        noImplicitAny: true,
-        target: "ES6"
-    })
-];
+const builder = new MinecraftAddonBuilder("Chess");
+builder.addPlugin(new TypeScriptSupport());
+builder.addPlugin(new BrowserifySupport());
+
 module.exports = builder.configureEverythingForMe();
